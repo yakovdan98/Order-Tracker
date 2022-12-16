65a1833f52a81c0eeb5110ccf6c5c73367983738
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-12-16 14:00:23 -0800

    added order creation page

---
+++ OrderTracker/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+
+namespace OrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{id}/orders/new")]
+    public ActionResult New(int id)
+    {
+      return View(Vendor.Find(id));
+    }
+  }
+}
